0a3b96646ad6cf11a2631c01168500184ef4cac0
Clément Poirier <p.clement777@gmail.com>
2020-03-13 14:04:25 +0100

    Write map in file result.out

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,8 +1,28 @@
 package com.hemebiotech.analytics;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class AnalyticsCounter {
+
+    public static void writeFileOut(Map<String, Integer> symptomsMap, String outPath) {
+        try {
+            File file = new File(outPath);
+            FileOutputStream fos = new FileOutputStream(file);
+            PrintWriter pw = new PrintWriter(fos);
+
+            for (Map.Entry<String, Integer> m : symptomsMap.entrySet()) {
+                pw.println(m.getKey() + "=" + m.getValue());
+            }
+            pw.flush();
+            pw.close();
+            fos.close();
+        } catch (Exception e) {
+            System.out.println("Problème dans la création ou dans l'écriture du fichier de sortie");
+        }
+    }
 
     public static Map<String, Integer> mapFromList(List<String> listFull) {
         Map<String, Integer> new_map = new HashMap<>();
@@ -16,6 +36,8 @@ public class AnalyticsCounter {
         ISymptomReader listSymptom = new ReadSymptomDataFromFile("symptoms.txt");
 
         Map<String, Integer> mapSymptoms = mapFromList(listSymptom.GetSymptoms());
+
+        writeFileOut(mapSymptoms, "result.out");
     }
 
 }
